25d3d80677e2b05d052edb9ef7bb37c62e646738
Jin Wang <80500906+Devin-Wong@users.noreply.github.com>
2024-09-06 17:32:18 +0000

    U

---
+++ Topic-1_Variables and Statements/Code/practice.py
@@ -0,0 +1,6 @@
+x = input("You can input two numbers. I will return the product the two numbers. \nPlease input the first number: ")
+y = input("Please input the second number: ")
+
+rst = float(x) * float(y)
+
+print(f"{x} times {y} equals {rst}")
