753401829d66458875918bbf53293a24769ec427
Devin-Wong <wangjin.devin@gmail.com>
2024-11-05 11:59:19 -0500

    U

--- Project-1/project-1.py
+++ Project-1/project-1.py
@@ -1,16 +1,20 @@
 # --- read a file to obatin a list of numbers ------
 import csv
-file_path = "project-1.csv"
-with open(file_path, mode='r') as file:
-    csv_reader = csv.reader(file)
-    header = next(csv_reader)
-    print("Header:", header)
-    spy = []
-    for row in csv_reader:
-            print(row)
-            spy.append(row[1])
 
-spy = list(map(float, spy))     
+file_path = "project-1.csv"
+
+def read_file(file_path):
+    with open(file_path, mode='r') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        spy = []
+        for row in csv_reader:
+                spy.append(row[1])
+
+    return list(map(float, spy)) 
+    
+spy = read_file(file_path)
+# print(spy)
 # ---------------------------------------------------
 
 
